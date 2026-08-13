4ff11bf44302c1043a700ca4ae8ecdb9b705f93d
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-08-13 18:05:52 -0400

    feat(semana-01): configura Gradle para ejemplo Java Kotlin

---
+++ semanas/semana-01/ejemplo-java-kotlin/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "cl.duoc.dsy1105"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
